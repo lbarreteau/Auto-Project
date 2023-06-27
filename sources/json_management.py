74af941f2facdeb591460f37852ac74a96e22c6e
Luchooooo <louis.barreteau@epitech.eu>
2023-06-27 13:51:05 +0200

    [ADD]: open, load, close for json config file

--- sources/json_management.py
+++ sources/json_management.py
@@ -7,8 +7,40 @@
 
 import json
 
-def open_json_config(file_name="project-config.json"):
+def close_json_config(json_file):
     try:
-        json_file = open("../" + file_name)
-    except:
+        json_file.close()
+    except Exception as e:
+        print("Could not close json file")
+        print(e)
+        return None
+    return True
+
+def load_json_config(json_file):
+    try:
+        json_data = json.load(json_file)
+    except Exception as e:
+        print("Could not load json data")
+        print(e)
+        return None
+    return json_data
+
+def open_json_config(file_name):
+    try:
+        json_file = open("../" + file_name, "r")
+    except Exception as e:
         print("Could not open configuration file")
+        print(e)
+        return None
+    return json_file
+
+def open_and_parse_json_config(file_name="project-config.json"):
+    json_file = open_json_config(file_name)
+    if json_file == None:
+        return None
+    json_data = load_json_config(json_file)
+    if json_data == None:
+        return None
+    if close_json_config(json_file) == None:
+        return None
+    return json_data
